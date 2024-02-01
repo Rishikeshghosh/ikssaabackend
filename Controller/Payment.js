890f4435f3bb4af768e5db89717fb6ec2b7fd4e6
Rishikeshghosh <rishikeshghoshghosh@gmail.com>
2024-02-01 10:51:00 +0530

    b

--- Controller/Payment.js
+++ Controller/Payment.js
@@ -4,7 +4,7 @@ export const newPayment = async (req, res) => {
   try {
     const merchantTransactionId = "MT7850590068188104"; //req.body.transactionId;
     const data = {
-      merchantId: "PGTESTPAYUAT",
+      merchantId: "ghosh.759",
       merchantTransactionId: merchantTransactionId,
       merchantUserId: "MUID123",
       name: req.body.name,
